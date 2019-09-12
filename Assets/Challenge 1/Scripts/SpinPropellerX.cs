6beff209bdc096a2a0eed5af393e710b34026118
jburi <jacobbob55@comcast.net>
2019-09-12 13:48:23 -0500

    Assignment 3

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,26 @@
+﻿/*
+* Jacob Buri
+* SpinPropellerX.cs
+* Assignment 3
+* Spins the propeller every frame
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    public Vector3 RotateAmount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateAmount * Time.deltaTime);
+    }
+}
